457630f18ff20660aa3b62cd80506f256dc655af
Ajith Antony <ajithpallisseryantony@gmail.com>
2026-02-03 22:40:51 +0400

    fix: remove Box wrappers from CountryCitySelect fields
    - Remove individual Box wrappers around country and state fields
    - Apply sx props directly to FormControl for flex sizing
    - Now renders as two FormControl elements in Fragment
    - Allows proper flex wrapping with other form fields

--- src/components/Forms/CountryCitySelect.tsx
+++ src/components/Forms/CountryCitySelect.tsx
@@ -5,11 +5,10 @@ import {
   Select,
   MenuItem,
   FormHelperText,
-  Box,
   CircularProgress,
 } from "@mui/material";
 import { useTranslation } from "react-i18next";
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import type { FieldConfig } from "../../pages/FormOne/helper";
 import { fetchCountries, fetchStates, type Country, type State } from "../../services/locationService";
 
@@ -79,108 +78,104 @@ export default function CountryCitySelect<T extends FieldValues>({
   }, [countryValue, countries]);
 
   return (
-    <Box sx={{ display: "flex", gap: 2, flexWrap: "wrap", width: "100%" }}>
+    <Fragment>
       {/* Country Select */}
-      <Box sx={{ minWidth: 250, flex: 1 }}>
-        <Controller
-          name={countryField.name as Path<T>}
-          control={control}
-          rules={countryField.rules}
-          render={({ field: controllerField, fieldState: { error } }) => {
-            const hasError = !!error;
-            const helperText = hasError ? t(error.message ?? "") : "";
-            const helperId = `${countryField.name}-helper`;
+      <Controller
+        name={countryField.name as Path<T>}
+        control={control}
+        rules={countryField.rules}
+        render={({ field: controllerField, fieldState: { error } }) => {
+          const hasError = !!error;
+          const helperText = hasError ? t(error.message ?? "") : "";
+          const helperId = `${countryField.name}-helper`;
 
-            return (
-              <FormControl fullWidth error={hasError}>
-                <InputLabel id={`${countryField.name}-label`}>
-                  {t(countryField.label)}
-                </InputLabel>
-                <Select
-                  {...controllerField}
-                  labelId={`${countryField.name}-label`}
-                  label={t(countryField.label)}
-                  aria-invalid={hasError}
-                  aria-describedby={helperId}
-                  disabled={loadingCountries}
-                  value={controllerField.value || ""}
-                  onChange={(e) => {
-                    controllerField.onChange(e);
-                    console.log("Country selected:", e.target.value);
-                  }}
-                >
-                  {loadingCountries ? (
-                    <MenuItem disabled>
-                      <CircularProgress size={20} />
+          return (
+            <FormControl sx={{ minWidth: 250, flex: 1 }} error={hasError}>
+              <InputLabel id={`${countryField.name}-label`}>
+                {t(countryField.label)}
+              </InputLabel>
+              <Select
+                {...controllerField}
+                labelId={`${countryField.name}-label`}
+                label={t(countryField.label)}
+                aria-invalid={hasError}
+                aria-describedby={helperId}
+                disabled={loadingCountries}
+                value={controllerField.value || ""}
+                onChange={(e) => {
+                  controllerField.onChange(e);
+                  console.log("Country selected:", e.target.value);
+                }}
+              >
+                {loadingCountries ? (
+                  <MenuItem disabled>
+                    <CircularProgress size={20} />
+                  </MenuItem>
+                ) : countries.length > 0 ? (
+                  countries.map((country) => (
+                    <MenuItem key={country.iso2} value={country.iso2}>
+                      {country.country}
                     </MenuItem>
-                  ) : countries.length > 0 ? (
-                    countries.map((country) => (
-                      <MenuItem key={country.iso2} value={country.iso2}>
-                        {country.country}
-                      </MenuItem>
-                    ))
-                  ) : (
-                    <MenuItem disabled>{t("common.noCountriesAvailable")}</MenuItem>
-                  )}
-                </Select>
-                {hasError && (
-                  <FormHelperText id={helperId}>{helperText}</FormHelperText>
+                  ))
+                ) : (
+                  <MenuItem disabled>{t("common.noCountriesAvailable")}</MenuItem>
                 )}
-              </FormControl>
-            );
-          }}
-        />
-      </Box>
+              </Select>
+              {hasError && (
+                <FormHelperText id={helperId}>{helperText}</FormHelperText>
+              )}
+            </FormControl>
+          );
+        }}
+      />
 
       {/* State Select */}
-      <Box sx={{ minWidth: 250, flex: 1 }}>
-        <Controller
-          name={stateField.name as Path<T>}
-          control={control}
-          rules={stateField.rules}
-          render={({ field: controllerField, fieldState: { error } }) => {
-            const hasError = !!error;
-            const helperText = hasError ? t(error.message ?? "") : "";
-            const helperId = `${stateField.name}-helper`;
+      <Controller
+        name={stateField.name as Path<T>}
+        control={control}
+        rules={stateField.rules}
+        render={({ field: controllerField, fieldState: { error } }) => {
+          const hasError = !!error;
+          const helperText = hasError ? t(error.message ?? "") : "";
+          const helperId = `${stateField.name}-helper`;
 
-            return (
-              <FormControl fullWidth error={hasError}>
-                <InputLabel id={`${stateField.name}-label`}>
-                  {t(stateField.label)}
-                </InputLabel>
-                <Select
-                  {...controllerField}
-                  labelId={`${stateField.name}-label`}
-                  label={t(stateField.label)}
-                  aria-invalid={hasError}
-                  aria-describedby={helperId}
-                  disabled={!countryValue || loadingStates}
-                  value={controllerField.value || ""}
-                >
-                  {loadingStates ? (
-                    <MenuItem disabled>
-                      <CircularProgress size={20} />
+          return (
+            <FormControl sx={{ minWidth: 250, flex: 1 }} error={hasError}>
+              <InputLabel id={`${stateField.name}-label`}>
+                {t(stateField.label)}
+              </InputLabel>
+              <Select
+                {...controllerField}
+                labelId={`${stateField.name}-label`}
+                label={t(stateField.label)}
+                aria-invalid={hasError}
+                aria-describedby={helperId}
+                disabled={!countryValue || loadingStates}
+                value={controllerField.value || ""}
+              >
+                {loadingStates ? (
+                  <MenuItem disabled>
+                    <CircularProgress size={20} />
+                  </MenuItem>
+                ) : states.length > 0 ? (
+                  states.map((state) => (
+                    <MenuItem key={state.state_code} value={state.name}>
+                      {state.name}
                     </MenuItem>
-                  ) : states.length > 0 ? (
-                    states.map((state) => (
-                      <MenuItem key={state.state_code} value={state.name}>
-                        {state.name}
-                      </MenuItem>
-                    ))
-                  ) : (
-                    <MenuItem disabled>
-                      {countryValue ? t("common.noStatesAvailable") : t("common.selectCountryFirst")}
-                    </MenuItem>
-                  )}
-                </Select>
-                {hasError && (
-                  <FormHelperText id={helperId}>{helperText}</FormHelperText>
+                  ))
+                ) : (
+                  <MenuItem disabled>
+                    {countryValue ? t("common.noStatesAvailable") : t("common.selectCountryFirst")}
+                  </MenuItem>
                 )}
-              </FormControl>
-            );
-          }}
-        />
-      </Box>
-    </Box>
+              </Select>
+              {hasError && (
+                <FormHelperText id={helperId}>{helperText}</FormHelperText>
+              )}
+            </FormControl>
+          );
+        }}
+      />
+    </Fragment>
   );
 }
